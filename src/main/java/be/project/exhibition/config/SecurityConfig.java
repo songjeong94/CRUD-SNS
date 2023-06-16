fa81efaa2a2d3cdfb75161b398edce2c1a5d3e3e
songjeong94 <thdwjd7899@naver.com>
2023-06-16 21:51:57 +0900

    #4 - 유저 회원가입시 password 암호화 추가 todo :  jwt 적용 과 security authenticaiton을 통하여 user정보 받아오기

--- src/main/java/be/project/exhibition/config/SecurityConfig.java
+++ src/main/java/be/project/exhibition/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,10 +14,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .cors().disable()
                 .csrf().disable()
                 .authorizeHttpRequests(request ->
                         request.anyRequest().permitAll())
                 .formLogin().and()
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
